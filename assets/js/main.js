75d0d4ddc86ac85b64a24b19253713e06e54e82c
pctcurtana <phamthat2206@gmail.com>
2021-12-29 17:00:18 +0700

    done trash web

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
-var header = document.getElementById('header');
-var pt = document.getElementsByClassName('pt');
-
-header.onclick = function() {
-    alert('dm thanh cong');
-}
+        var header = document.getElementById('header');
+        var menu = document.getElementById('menu');
+        menu.onclick = function() {
+            var isClosed = header.clientHeight === 50;
+            if (isClosed) {
+                header.style.height = '100%';
+            } else {
+                header.style.height = '50px';
+            }
+        }
+        var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+        for (var i = 0; i < menuItems.length; i++) {
+            var menuItem = menuItems[i];
+            
+            menuItem.onclick = function() {
+                header.style.height = '50px';
+            }
+        };
+        var typing = document.getElementById('typing');
+        var typed = new Typed("#typing", {
+            strings: ["Newbie", "Programming Learners", "Freelancer"],
+            typeSpeed: 110,
+            backSpeed: 70,
+            loop: true
+        });
+        alert  ('ok')
